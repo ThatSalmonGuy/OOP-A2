6335debd545768e9af0e7f8cc7a55053f3a1ebe9
Luke James Salmon <lukesalmon2014@hotmail.com>
2023-04-27 02:30:58 +0100

    requirements met

--- OOP A2 - LinCode Math Tutorial Program/Card.cs
+++ OOP A2 - LinCode Math Tutorial Program/Card.cs
@@ -3,72 +3,167 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace OOP_A2___LinCode_Math_Tutorial_Program
 {
-    internal class Card
+    public abstract class Card : Pack
     {
-        static void Main(string[] args)
+        static public void Main(string[] args)
         {
-            bool validInput = true;
-            string userInput = null;
-            
-            Console.WriteLine("Welcome to LinCode's Year 1 and 2 Maths tutoring program!");
+            ShowMainMenu(args);
+        }
+
+        static void ShowMainMenu(string[] args)
+        {
+            Console.WriteLine("Welcome to LinCode's Year 1 and 2 Maths tutoring program main menu!");
             Console.WriteLine("");
             Console.WriteLine("Please Select one the following options to proceed by entering the corresponding number.");
             Console.WriteLine("");
             Console.WriteLine("1. Program Tutorial");
-            Console.WriteLine("2. Beginner Question Difficulty");
-            Console.WriteLine("3. Harder Question Difficulty");
-            Console.WriteLine("4. Exit Application");
+            Console.WriteLine("2. Maths Question Generator");
+            Console.WriteLine("3. Exit Application");
             Console.WriteLine("");
 
+            string userInput = Console.ReadLine();
 
-            userInput = Console.ReadLine();
-
-
-            while (validInput == true)
+            switch (userInput)
             {
-                if (userInput != "1" && userInput != "2" && userInput != "3" && userInput != "4")
-                {
-                    Console.WriteLine("Invalid input. The input must be 1, 2, 3, or 4.");
+                case "1":
+                    Tutorial.TutorialMain(args);
                     break;
-                }
-                else
-                {
-                    if (userInput == "1")
-                    {
 
-                        Tutorial.tutorialMain(args);
-                        break;
-                    }
+                case "2":
+                    MathsQuestionGenerator(args);
+                    break;
 
-                    if (userInput == "2")
-                    {
-                        break;
-                        //Beginner Question Difficulty
-                    }
+                case "3":
+                    Environment.Exit(0);
+                    break;
 
-                    if (userInput == "3")
-                    {
-                        break;
-                        //Intermediate Difficulty
-                    }
-
-                    if (userInput == "4")
-                    {
-                        Console.WriteLine("Thank you for using our application and we hope you learned lots!"); //Exit Message
-                        Environment.Exit(0); //Exits Application
-                    }
-
-
-                }
+                default:
+                    Console.Clear();
+                    Console.WriteLine("Invalid input. The input must be 1, 2, 3, or 4.");
+                    Console.WriteLine("");
+                    Console.WriteLine("Press any key to return to main menu");
+                    Console.WriteLine("");
+                    Console.ReadLine();
+                    Console.Clear();
+                    ShowMainMenu(args);
+                    break;
             }
-            userInput = null;
         }
 
+        static void ShowTutorial(string[] args)
+        {
+            Console.Clear();
+            Tutorial.TutorialMain(args);
+            ShowMainMenu(args);
+        }
 
+        static void MathsQuestionGenerator(string[] args)
+        {
+            Console.Clear();
+            Pack card = new Pack();
+            int randomNumOne = card.NumberGeneratorOne(); //tests the ability to call the first number generator from Pack
+            int randomNumTwo = card.NumberGeneratorTwo(); //tests the ability to call the second number generator from Pack
+            string randomOperationOne = card.OperationGeneratorOne();
+
+            int correctAnswer = 0;
+
+            switch (randomOperationOne)
+            {
+                case "+":
+                    correctAnswer = randomNumOne + randomNumTwo;
+                    break;
+                case "-":
+                    correctAnswer = randomNumOne - randomNumTwo;
+                    break;
+                case "/":
+                    correctAnswer = randomNumOne / randomNumTwo;
+                    break;
+                case "*":
+                    correctAnswer = randomNumOne * randomNumTwo;
+                    break;
+            }
+
+            Console.WriteLine("What is...");
+            Console.WriteLine(randomNumOne + "" + randomOperationOne + "" + randomNumTwo + "?");
+            Console.WriteLine("");
+
+            string userAnswer = Console.ReadLine();
+            string correctAnswerString = correctAnswer.ToString();
+
+            if (userAnswer == correctAnswerString)
+            {
+                Console.Clear();
+                Console.WriteLine("Correct answer! Congratulations!");
+                Console.WriteLine("");
+                Console.WriteLine("1. New Question");
+                Console.WriteLine("2. Exit to main menu");
+                Console.WriteLine("");
+
+                string continueOrMenuInput = Console.ReadLine();
+
+                while (true)
+                {
+                    if (continueOrMenuInput == "1")
+                    {
+                        MathsQuestionGenerator(args);
+                        break;
+                    }
+                    else if (continueOrMenuInput == "2")
+                    {
+                        Console.Clear();
+                        ShowMainMenu(args);
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter a valid input of 1 or 2");
+                        Console.WriteLine("");
+                        continueOrMenuInput = Console.ReadLine();
+                    }
+                }
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Unfortunately that is the incorrect answer!");
+                Console.WriteLine("");
+                Console.WriteLine("Press 1 for a new question");
+                Console.WriteLine("Press 2 to return to main menu");
+                Console.WriteLine("");
+
+                string continueOrMenuInput = Console.ReadLine();
+
+                while (true)
+                {
+                    if (continueOrMenuInput == "1")
+                    {
+                        MathsQuestionGenerator(args);
+                        break;
+                    }
+                    else if (continueOrMenuInput == "2")
+                    {
+                        Console.Clear();
+                        ShowMainMenu(args);
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter a valid input of 1 or 2");
+                        Console.WriteLine("");
+                        continueOrMenuInput = Console.ReadLine();
+                    }
+                }
+
+            }
+        }
+
+       
+        }
     }
-}
+
